63e17b7fd6a706ba08245364c573dd7fffe48d3d
András Ács <acsandras@gmail.com>
2019-09-09 09:45:16 +0200

    Initial commit

---
+++ src/Variabler.java
@@ -0,0 +1,49 @@
+/**
+ * Developed by András Ács (acsandras@gmail.com)
+ * Zealand / www.zealand.dk
+ * Licensed under the MIT License
+ * 2019-09-09
+ */
+
+public class Variabler {
+
+    public static void main(String[] args) {
+
+        // Primitive datatyper
+        byte a = 127;
+        short c = 1001;
+        int b = 10024;
+
+        b = a + c;
+
+        // Decimaltal
+        double d = 2.7;
+        float e = 5.0f;
+
+        // Boolean
+        boolean f = true;
+        boolean g = false;
+
+        // String
+        String h = "Pølse";
+
+        // Char
+        char i = 'b';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+        System.out.println(a+b);
+        System.out.println(a+d);
+        System.out.println(a+h);
+        
+
+    }
+
+}
